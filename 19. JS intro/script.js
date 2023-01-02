3415c67d4249bb4dac2dfadc4d1956f221c348a6
udaykiranchinnichanti <udaykiranchinnichanti@gmail.com>
2023-01-02 17:43:59 +0530

    Java Script basics

---
+++ 19. JS intro/script.js
@@ -0,0 +1,43 @@
+// Log
+console.log(1224);
+/*Alert id
+asjkdhiuahsdh
+*/
+alert(56 + 3123);
+// ES5
+var age = '25'; //Variable
+age = "26"; // Reassign
+console.log(age);
+
+// ES6
+let firstName = "UDAY";
+firstName = "KIRAN";
+console.log(firstName);
+
+const gender = "Male";
+
+// gender = "Female" // We should not reassign const variable
+console.log(gender)
+
+let lastName = "UDAY";
+
+// String concatination
+
+let fullName = firstName + ' ' + lastName;  
+
+
+console.log(fullName);
+
+// Case sensitive
+const Gender = ""
+
+
+
+/* Naming Rules:
+    1. No JS keyword - i.e., const var = "ASASD" - Mandatory
+    2. name should not start with numbers - i.e., let 3name = "ANKANDS" - Mandatory
+    3. No space and special character in variable name i.e., let first Name = ""; let first/Name = "";  - Mandatory
+    4. Case sensitive
+    5. Variable name should be meaningful - Best practice
+    6. Should use camelCase i.e., firstName., first letter of first word will be small and next word should be Capital
+*/
