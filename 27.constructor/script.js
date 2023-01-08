d08ed206e3b416f1e2f1cbdfb8a21c47166a97f9
udaykiranchinnichanti <udaykiranchinnichanti@gmail.com>
2023-01-08 09:16:29 +0530

    constructor

---
+++ 27.constructor/script.js
@@ -0,0 +1,56 @@
+
+
+let user = (name, dob) => {
+    console.log(name)
+    console.log(dob)
+    return {
+        name: name,
+        dob: dob,
+        getMyAge: () => {
+            // console.log(user(name, dob).dob, "DOB INSIDE Get my age function")
+            // console.log(user().dob)
+            let dobValue = user(name, dob).dob;
+            let getYear = new Date(dobValue).getFullYear();
+            let currentYear = new Date().getFullYear();
+            // console.log(getYear, "Get full year")
+            // console.log(getYear, currentYear)
+            let myAge = currentYear - getYear;
+            return myAge
+        }
+    }
+}
+
+console.log(user("Ananth", "2001-05-14").getMyAge());
+
+// Constructor name should be in Pascal case
+
+// function User(name, dob) {
+//     this.name = name
+//     this.dob = dob
+// }
+function User(name, dob) {
+    this.name = name
+    this.dob = dob
+    this.getMyAge = () => {
+        let dobValue = this.dob;
+        let getYear = new Date(dobValue).getFullYear();
+        let currentYear = new Date().getFullYear();
+        let myAge = currentYear - getYear;
+        return myAge
+    }
+}
+
+let person1 = new User("Anwar", "2001-05-14");
+
+// this = { name: "Anwar",  dob: "2001-05-14", getMyAge: () => {
+//     let dobValue = this.dob;
+//     let getYear = new Date(dobValue).getFullYear();
+//     let currentYear = new Date().getFullYear();
+//     let myAge = currentYear - getYear;
+//     return myAge
+// }}
+console.log(person1.name, "Name");
+console.log(person1.getMyAge(), "Constructor age");
+
+// In constructor, the values are called as Property other than function
+// In constructor, the functions are called as Methods
